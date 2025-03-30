a2d623dce8b167c2b47b27e4ecc6f3a9b8789139
Ricardo Lopes <ricardo.lopes@ifood.com.br>
2025-03-30 13:33:13 -0300

    Add request o exchange API on cotacao route

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -18,7 +20,43 @@ func main() {
 func handleCotacao(w http.ResponseWriter, r *http.Request) {
 	log.Default().Println("Received request for /cotacao")
 
+	cotacao, err := FindCotacao()
+	if err != nil {
+		log.Default().Printf("Error fetching cotacao: %v", err)
+		http.Error(w, "Failed to fetch cotacao", http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"cotacao": 5.25}`))
+	if err := json.NewEncoder(w).Encode(cotacao); err != nil {
+		log.Default().Printf("Error encoding response: %v", err)
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func FindCotacao() (interface{}, error) {
+	url := "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch data: %s", res.Status)
+	}
+	var data interface{}
+	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
