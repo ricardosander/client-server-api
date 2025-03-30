9c817e16f8dd8403c86ca3d64c111f68cf5d21f1
Ricardo Lopes <ricardo.lopes@ifood.com.br>
2025-03-30 15:02:40 -0300

    Write server response to file

--- client.go
+++ client.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -12,6 +13,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		panic(err)
@@ -19,15 +21,39 @@ func main() {
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
+		fmt.Println("Error:", res.Status)
 		panic(fmt.Errorf("failed to fetch data: %s", res.Status))
 	}
+
 	var data PriceResponse
 	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+		fmt.Println("Error decoding response:", err)
 		panic(err)
 	}
-	fmt.Printf("%+v\n", data)
+
+	err = SaveToFile(data)
+	if err != nil {
+		fmt.Println("Error saving to file:", err)
+		panic(err)
+	}
+
+	fmt.Printf("Value %s retrieve from server and saved to file\n", data.Value)
 }
 
 type PriceResponse struct {
 	Value string `json:"bid"`
+}
+
+func SaveToFile(price PriceResponse) error {
+	fmt.Printf("Saving to file: %s\n", price.Value)
+
+	file, err := os.Create("cotacao.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	newLine := fmt.Sprintf("Dólar: %s\n", price.Value)
+	_, err = file.WriteString(newLine)
+	return err
 }
